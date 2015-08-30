9515699b27003d69f47174b5a95b00d7a0f3316b
KageshiMiyagawa <songofwheel8@gmail.com>
2015-08-30 00:07:07 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoYu::Application.config.secret_key_base = 'ee7efd1d3ce54649769f3a796cea8c9f15e83ec406575f3c9e4b569e73236017b613937b09a1178d9e7734574f006fb2ad597f22ef805e2eab16c11b0bc23472'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShoYu::Application.config.session_store :cookie_store, key: '_sho-yu_session'
